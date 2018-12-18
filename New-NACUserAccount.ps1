d82f513422d00a5150fb29f989c361e0808ccced
mukn <45763644+mukn@users.noreply.github.com>
2018-12-18 11:12:30 -0500

    Create New-NACUserAccount.ps1

---
+++ New-NACUserAccount.ps1
@@ -0,0 +1,75 @@
+#
+.SYNOPSIS
+New-NACUserAccount creates new user accounts and assigns those users to the appropriate users based on their job role.
+.DESCRIPTION
+
+.PARAMETER FirstName
+The first name (given name) of the user.
+
+.PARAMTER LastName
+The last name (surname) of the user.
+
+.PARAMETER RanNum
+The randomly assigned four digits to be used with the user's account.
+
+.PARAMETER Division
+The organizational division to be used to define OU as well as manager.
+
+.PARAMETER Manager
+If declared, this overrides the manager assigned by $Division declaration.
+
+.EXAMPLE
+.\New-NACUserAccount.ps1 -FirstName 'Chris' -LastName 'Kaufman' -RanNum '1234' -Division 'Sales'
+
+#>
+
+# Set parameters.
+param {
+	[Parameter(Mandatory=$True)]
+  [Alias('First')]
+  [string]$FirstName,
+	[Parameter(Mandatory=$True)]
+  [Alias('Last')]
+  [string]$LastName,
+	[Parameter(Mandatory=$True)]
+  [Alias('SSN')]
+  [Alias('Date')]
+  [int]$FourDigits,
+	[Parameter(Mandatory=$True)]
+  [string]$Division,
+  [Parameter(Mandatory=$False)]
+  [string]$Manager,
+  [Parameter(Mandatory=$False)]
+  [string]$EmployeeNumber,
+  [Parameter(Mandatory=$False)]
+  [string]$MobilePhone
+	}
+
+# Set other variables.
+$UserName = "$FirstName.$LastName"
+$UserEmail = "$UserName@nacgroup.com"
+$UserTemplate = Get-ADUser -Identity "$Division.Template"
+$ManagerCN = Get-ADUser -Identity $UserTemplate -Properties Manager
+$ProfilePath = ($UserTemplate.DistinguishedName -split ",",2)[1]
+
+# Initial user account creation.
+New-ADUser -SamAccountName $UserName -Name "$FirstName $LastName"
+
+# Set user properties: Description, DisplayName, EmailAddress, EmployeeNumber, GivenName, Manager, MobilePhone, Surname, UserPrincipalName.
+Set-ADUser -Identity $Username -Description "$Division"
+Set-ADUser -Identity $Username -DisplayName "$FirstName $LastName"
+Set-ADUser -Identity $Username -EmailAddress "$UserName@nacgroup.com"
+Set-ADUser -Identity $UserName -GivenName $FirstName
+Set-ADUser -Identity $UserName -Manager $ManagerCN
+Set-ADUser -Identity $UserName -Surname $LastName
+Set-ADUser -Identity $UserName -UserPrincipalName "$FirstName.$LastName@nacgroup.com"
+
+# Move to correct OU.
+Get-ADUser -Identity $UserName | Move-ADObject -TargetPath $ProfilePath
+
+# Set proxy address(es).
+Get-ADUser -Identity $UserName | Set-ADUser -Add @{ProxyAddresses="SMTP:$UserEmail"}
+
+# Reset user password and enable account.
+Set-ADAccountPassword -Identity $UserName -Reset
+Set-ADUser -Identity $UserName -Enabled $True
