ab2d795af9d1a297856052e63add651ae947bad2
mukn <45763644+mukn@users.noreply.github.com>
2019-02-06 13:16:42 -0500

    Create script

--- Terminate-NACUserAccount.ps1
+++ Terminate-NACUserAccount.ps1
@@ -33,8 +33,119 @@ param (
   $Preserve=$False
 )
 
-# Reset user password to random value. First create a random password.
-# Taken from https://blogs.technet.microsoft.com/heyscriptingguy/2013/06/03/generating-a-new-password-with-windows-powershell/
+<# Functions in the script.DESCRIPTION
+Function Send-EmailMessage () {
+  param (
+    [Parameter(Mandatory=$False)]
+    [string]$Recipients="helpdesk@nacgroup.com",
+    [Parameter(Mandatory=$False)]
+    [string]$ReportTitle,
+    [Parameter(Mandatory=$False)]
+    [string]$HTMLContent
+  )
+
+  #region Variables and Arguments
+  $fromemail = "noyes-information@nacgroup.com"
+  $server = "smtp.office365.com" #enter your own SMTP server DNS name / IP address here
+
+  #Internal settings for email.
+  $SourceMailbox = "noyes-information@nacgroup.com"
+  $ReceiveMailbox = "$UserManagerEmail"
+  $SourceMailboxPasswordSecure = ".\001Ref-SecureString.txt"
+  $Creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $SourceMailbox,(Get-Content $SourceMailboxPasswordSecure | ConvertTo-SecureString)
+  $ReportTitle = "User account terminated ($UserName)"
+
+  #endregion
+
+  # Assemble the HTML Header and CSS for our Report
+  $HTMLHeader = @"
+  <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
+  <html><head><title>$ReportTitle</title>
+  <style type="text/css">
+  <!--
+  body {
+  font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;
+  }
+
+      #report { width: 835px; }
+
+      table{
+  	border-collapse: collapse;
+  	border: none;
+  	font: 10pt Verdana, Geneva, Arial, Helvetica, sans-serif;
+  	color: black;
+  	margin-bottom: 10px;
+  }
+
+      table td{
+  	font-size: 12px;
+  	padding-left: 0px;
+  	padding-right: 20px;
+  	text-align: left;
+  }
+
+      table th {
+  	font-size: 12px;
+  	font-weight: bold;
+  	padding-left: 0px;
+  	padding-right: 20px;
+  	text-align: left;
+  }
+
+  h2{ clear: both; font-size: 130%; }
+
+  h3{
+  	clear: both;
+  	font-size: 115%;
+  	margin-left: 20px;
+  	margin-top: 30px;
+  }
+
+  p{ margin-left: 20px; font-size: 12px; }
+
+  table.list{ float: left; }
+
+      table.list td:nth-child(1){
+  	font-weight: bold;
+  	border-right: 1px grey solid;
+  	text-align: right;
+  }
+
+  table.list td:nth-child(2){ padding-left: 7px; }
+  table tr:nth-child(even) td:nth-child(even){ background: #CCCCCC; }
+  table tr:nth-child(odd) td:nth-child(odd){ background: #F2F2F2; }
+  table tr:nth-child(even) td:nth-child(odd){ background: #DDDDDD; }
+  table tr:nth-child(odd) td:nth-child(even){ background: #E5E5E5; }
+  div.column { width: 320px; float: left; }
+  div.first{ padding-right: 20px; border-right: 1px  grey solid; }
+  div.second{ margin-left: 30px; }
+  table{ margin-left: 20px; }
+  -->
+  </style>
+  </head>
+  <body>
+
+  @
+
+  # Create HTML body for the report.
+  $HTMLMiddle = "The password for $UserName was reset to $PlainPassword. Please log into the user mailbox (https://outlook.office.com) and OneDrive to identify any data that needs immediate attention. Mail will be forwarded to you for 30 days. After 30 days, the account will be marked for deletion. After 60 days, the account will be deleted."
+
+  # Assemble the closing HTML for our report.
+  $HTMLEnd = @
+  </div>
+  </body>
+  </html>
+  @
+
+  # Assemble the final report from all our HTML sections
+  $HTMLmessage = $HTMLHeader + $HTMLContent + $HTMLMiddle + $HTMLEnd
+  # Save the report out to a file in the current path
+  $HTMLmessage | Out-File ((Get-Location).Path + "\report.html")
+  # Email our report out
+  Send-MailMessage -To $ReceiveMailbox,$Recipients -Subject "[Report] $ReportTitle" -BodyAsHTML -Body $HTMLmessage -Attachments $ListOfAttachments -UseSsl -Port 587 -SmtpServer smtp.office365.com -From $SourceMailbox -Credential $Creds
+}
+#>
+
 Function Get-Password () {
   param (
     [int]$Length=12,
@@ -45,6 +156,9 @@ Function Get-Password () {
   }
   return $Password
 }
+
+# Reset user password to random value. First create a random password.
+# Taken from https://blogs.technet.microsoft.com/heyscriptingguy/2013/06/03/generating-a-new-password-with-windows-powershell/
 $SourceData = $NULL; For ($a = 48; $a –le 122; $a++) { $SourceData+=, [char][byte]$a }
 $PlainPassword = Get-Password -Length 12 -SourceData $SourceData
 # Convert password to secure string.
@@ -83,7 +197,7 @@ Foreach ( $Group in $UserGroups ) {
 
 # Remove user from GAL.
 # There is no Connect-ExchangeOnline cmdlt at this point and no method of
-# opening a nested PSSession per below. Removal is manual at this point.
+# opening a nested PSSession per below. Removal from GAL is manual at this point.
 # Enter-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $Admin365Credentials -Authentication Basic -AllowRedirection
 # Forward email messages to user's manager.
 
@@ -95,6 +209,7 @@ Remove-SPOUser -LoginName $UserName -Site https://nacgroup.sharepoint.com
 
 ## Follow up actions.
 # After 15 days, send a warning message to user's manager to review before deletion date.
+# Send-EmailMessage
 
 # After 30 days, remove Office 365 license.
 # User first needs to be assigned a region, then a license.
