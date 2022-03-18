c75582744b005ba39e15d4db6482f635bd1c03cc
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2022-03-18 11:52:33 -0400

    Update Get-BitLockerRecoveryKey.ps1

--- Get-BitLockerRecoveryKey.ps1
+++ Get-BitLockerRecoveryKey.ps1
@@ -5,10 +5,10 @@ Taken from https://stackoverflow.com/questions/50411539/retrieving-bitlocker-rec
 $TestOU = "OU=ABC,DC=XYZ,DC=com"
 $PCs = Get-ADComputer -Filter * -SearchBase $TestOU
 $Results = ForEach ($Computer in $PCs) {
-	New-Object PSObject -Property 
-		@{
-			ComputerName = $Computer.Name
-			RecoveryPassword = Get-ADObject -Filter 'objectclass -eq "msFVE-RecoveryInformation"' -SearchBase $computer.DistinguishedName -Properties msFVE-RecoveryPassword,whencreated | sort whencreated -Descending | select -expandProperty msfve-recoverypassword
-		}
-	}
+  New-Object PSObject -Property 
+    @{
+      ComputerName = $Computer.Name
+      RecoveryPassword = Get-ADObject -Filter 'objectclass -eq "msFVE-RecoveryInformation"' -SearchBase $computer.DistinguishedName -Properties msFVE-RecoveryPassword,whencreated | sort whencreated -Descending | select -expandProperty msfve-recoverypassword
+    }
+  }
 $Results
